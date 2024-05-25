3939bf51e91e894d96337e7e45e3f3d3c198a5ed
Ken Kan <kenhkan@gmail.com>
2024-05-24 23:39:02 -0400

    Test without CUDA

--- app/Main.hs
+++ app/Main.hs
@@ -2,17 +2,26 @@ module Main (main) where
 
 import Data.Array.Accelerate                    as A
 import qualified Data.Array.Accelerate.LLVM.PTX as PTX
+import qualified Data.Array.Accelerate.Interpreter as AI
 import qualified Foreign.CUDA.Driver            as C
 
 dotp :: Acc (Vector Float) -> Acc (Vector Float) -> Acc (Scalar Float)
 dotp xs ys = A.fold (+) 0 (A.zipWith (*) xs ys)
 
 main = do
+  let xs = fromList (Z:.10) [0..]   :: Vector Float
+      ys = fromList (Z:.10) [1,3..] :: Vector Float
+  -- Commenting this out would get rid of the segfault.
+  cuda xs ys
+  noCuda xs ys
+  putStrLn "*** complete ***"
+
+cuda xs ys = do
   C.initialise []
   dev0 <- C.device 0
   dev0Props <- C.props dev0
   ptx <- PTX.createTargetForDevice dev0 dev0Props []
-  let xs = fromList (Z:.10) [0..]   :: Vector Float
-      ys = fromList (Z:.10) [1,3..] :: Vector Float
   putStrLn $ show $ A.toList $ PTX.runWith ptx (dotp (use xs) (use ys))
-  putStrLn "*** complete ***"
+
+noCuda xs ys = do
+  putStrLn $ show $ A.toList $ AI.run (dotp (use xs) (use ys))
